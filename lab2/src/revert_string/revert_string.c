940267de39ca09fdc80b21d12fd9699f3c1bb323
julia558 <cuvu558@gmail.com>
2020-03-21 20:50:39 +0000

    add method revert_string

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -11,16 +11,5 @@ void RevertString(char *str)
     }
     for (i = 0; i < length; i++)
         str[i]=s[i];
-
-    //int N=strlen(str);
-    //char* a=malloc(sizeof(char)*N+1);
-    //int i;
-    //for (i=0;i<N;i++){
-    //    a[i]=str[N-i-1];
-    //}
-    //for(i=0;i<N;i++){
-    //    str[i]=a[i];
-    //}
-
 }
 
