caf186dd68d4595e9f9ea1b8b5ca97fa72d478d4
julia558 <cuvu558@gmail.com>
2020-04-12 20:02:05 +0000

    add task 1

--- lab4/src/parallel_min_max.c
+++ lab4/src/parallel_min_max.c
@@ -17,15 +17,31 @@
 #include "find_min_max.h"
 #include "utils.h"
 
+//Функция, вызываемая при генерации сигнала SIGALRM
+pid_t * children;
+int pnum;
+void MyAlarm (){
+    int i, status;
+    for(i=0;i<pnum;i++){
+        int res=waitpid(children[i], &status, WNOHANG);
+        if(res==0){
+            kill(children[i], SIGKILL);
+            printf("process %d was killed\n", children[i]);
+        }
+        else {
+            printf("process %d was finished\n", children[i]);
+        }
+    }
+}
+
 int main(int argc, char **argv) {
   int seed = -1;
   int array_size = -1;
   int pnum = -1;
   bool with_files = false;
-
+  int timeout = -1, f;
   while (true) {
     int current_optind = optind ? optind : 1;
-
     static struct option options[] = {{"seed", required_argument, 0, 0},
                                       {"array_size", required_argument, 0, 0},
                                       {"pnum", required_argument, 0, 0},
@@ -42,32 +58,38 @@ int main(int argc, char **argv) {
         switch (option_index) {
           case 0:
             seed = atoi(optarg);
-            // your code here
-            // error handling
+            // your code here  // error handling
             if (seed <= 0) {
                 printf("seed is a positive number\n");
                 return 1;}
             break;
           case 1:
             array_size = atoi(optarg);
-            // your code here
-            // error handling
+            // your code here // error handling
             if (array_size <= 0) {
                 printf("array_size is a positive number\n");
                 return 1;}
             break;
           case 2:
             pnum = atoi(optarg);
-            // your code here
-            // error handling
+            // your code here // error handling
             if (pnum <= 0) {
                 printf("pnum is a positive number\n");
                 return 1;}
             break;
           case 3:
+            f = atoi(optarg);
+            if((f<0)||(f>1)){
+                printf("by_files is a positive number\n");
+                return -1;}
             with_files = true;
             break;
-
+          case 4:
+            timeout = atoi(optarg);
+            if(timeout<=0){
+                printf("timeout is a positive number\n");
+                return 1;
+            }
           defalut:
             printf("Index %d is out of options\n", option_index);
         }
@@ -97,21 +119,21 @@ int main(int argc, char **argv) {
   GenerateArray(array, array_size, seed);
   int active_child_processes = 0;
 
-    // Время начала
+  // Время начала
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
 
-// Файл
-  FILE *f;
+  // Файл
+  FILE *fi;
   char name[] = "min_max.txt";
-  if ((f = fopen(name, "w")) == NULL) {
+  if ((fi = fopen(name, "w")) == NULL) {
     printf("Failed to create file");
     getchar();
     return -1;
   }
-  int file = fileno(f);
+  int file = fileno(fi);
 
-    // Pipe
+  // Pipe
   int file_pipe_min[2];
   int file_pipe_max[2];
   if (pipe(file_pipe_min)<0)
@@ -120,8 +142,7 @@ int main(int argc, char **argv) {
     exit(0);
 
   int i;
-  for (i = 0; i < pnum; i++) {
-      // child_pid - идентификатор процесса
+  for (i = 0; i < pnum; i++) {  // child_pid - идентификатор процесса
     pid_t child_pid = fork();
     if (child_pid >= 0) {
       // successful fork
@@ -132,43 +153,49 @@ int main(int argc, char **argv) {
         // child process
         struct MinMax min_max = GetMinMax(array,0,array_size);
         // parallel somehow
-
         char bufmin[100];
         sprintf(bufmin, "%d\n", min_max.min);
         char bufmax[100];
         sprintf(bufmax, "%d\n", min_max.max);
         
-        if (with_files) {
-          // use files here
-          // Установить эксклюзивную блокировку для записи
-          flock(file, LOCK_EX);  
-          write(file, (void*)bufmin, strlen(bufmin));
-          write(file, (void*)bufmax, strlen(bufmax));
-          //Снять блокировку         
-          flock(file, LOCK_UN);
-          fclose(f);
-        } else {
-          // use pipe here
-          char buf[100];
-          sprintf(buf,"%d",min_max.min);
-          write(file_pipe_min[1],buf,30);
-          sprintf(buf,"%d",min_max.max);
-          write(file_pipe_max[1],buf,30);
+        if (with_files) { // use files here
+            // Установить эксклюзивную блокировку для записи
+            flock(file, LOCK_EX);
+            write(file, (void*)bufmin, strlen(bufmin));
+            write(file, (void*)bufmax, strlen(bufmax));
+            //Снять блокировку
+            flock(file, LOCK_UN);
+            fclose(f);
+        } 
+        else { // use pipe here
+            char buf[100];
+            sprintf(buf,"%d",min_max.min);
+            write(file_pipe_min[1],buf,30);
+            sprintf(buf,"%d",min_max.max);
+            write(file_pipe_max[1],buf,30);
         }
         return 0;
       }
-
-    } else {
+    } 
+    else {
       printf("Fork failed!\n");
       return 1;
     }
   }
 
-  int status;
-  while (active_child_processes > 0) {
-    // your code here
-    wait(&status);
-    active_child_processes -= 1;
+ if (timeout != -1) {
+    //Привязка функции MyAlarm к сигналу SIGALRM
+    signal(SIGALRM, MyAlarm);
+    //Установка будильника для запуска MyAlarm
+    alarm(timeout);
+    pause();
+  } 
+  else {
+        while (active_child_processes > 0) {
+            // your code here
+            wait(0);
+            active_child_processes -= 1;
+        }
   }
 
   struct MinMax min_max;
@@ -179,33 +206,27 @@ int main(int argc, char **argv) {
     int min = INT_MAX;
     int max = INT_MIN;
 
-    if (with_files) {
-      // read from files
-      FILE *f = fopen("min_max.txt","r");
-      char buf[100];
-      int a;
-      fscanf(f,"%s",buf);
-      a=atoi(buf);
-      if (min>a) 
-        min=a;
-      fscanf(f,"%s",buf);
-      a=atoi(buf);
-      if (max<a) 
-        max=a;
-    } else {
-      // read from pipes
-      char buf[100];
-      int a;
-      read(file_pipe_min[0],buf,30);
-      a=atoi(buf);
-      if (min>a) 
-        min=a;
-      read(file_pipe_max[0],buf,30);
-      a=atoi(buf);
-      if (max<a) 
-        max=a;
+    if (with_files) { // read from files
+        FILE *f = fopen("min_max.txt","r");
+        char buf[100];
+        int a;
+        fscanf(f,"%s",buf);
+        a=atoi(buf);
+        if (min>a) min=a;
+        fscanf(f,"%s",buf);
+        a=atoi(buf);
+        if (max<a) max=a;
+    } 
+    else { // read from pipes
+        char buf[100];
+        int a;
+        read(file_pipe_min[0],buf,30);
+        a=atoi(buf);
+        if (min>a) min=a;
+        read(file_pipe_max[0],buf,30);
+        a=atoi(buf);
+        if (max<a) max=a;
     }
-
     if (min < min_max.min) min_max.min = min;
     if (max > min_max.max) min_max.max = max;
   }
